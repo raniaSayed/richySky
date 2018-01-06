632d49096e89071f610f007650868893901bc3df
raniaSayed <raniasayed313@gmail.com>
2018-01-07 01:18:37 +0200

    some changes

--- js/game.js
+++ js/game.js
@@ -1,79 +1,96 @@
-var char2 = document.getElementById("char2");
-char2.style.right="10px";
-
-char2.addEventListener("mouseenter",function(){
-    char2.style.WebkitTransform = "rotate(20deg)";
+var coin = document.getElementById("coin");
+var dollarCoin = document.getElementById("dollarCoin");
+coin.style.right="10px";
+rotate  = 0;
+flag = 0;
+var x=0;
+var intervalID;
+coin.addEventListener("mouseenter",function(){
+    if(!flag){
+    var intervalID = setInterval(function () {
+        // Your logic here
+        rotate += 20;
+        coin.style.WebkitTransform = "rotate("+rotate+"deg)"; 
+        flag =1;
+        if (x++ === 18) {
+            console.log(x);
+            window.clearInterval(intervalID);
+             coin.style.WebkitTransform = "rotate(0deg)";
+            flag = 0;
+            x = 0;
+        }
+     }, 100);
+}
 });
-char2.addEventListener("mouseleave",function(){
-    char2.style.WebkitTransform = "rotate(0deg)";
+coin.addEventListener("mouseleave",function(){
+     window.clearInterval(intervalID);
+    coin.style.WebkitTransform = "rotate(0deg)";
+
+});
+dollarCoin.style.right="10px";
+
+dollarCoin.addEventListener("mouseenter",function(){
+    if(!flag){
+    var intervalID = setInterval(function () {
+        // Your logic here
+        rotate += 20;
+        dollarCoin.style.WebkitTransform = "rotateY("+rotate+"deg)"; 
+        flag =1;
+        if (x++ === 18) {
+            console.log(x);
+            window.clearInterval(intervalID);
+             dollarCoin.style.WebkitTransform = "rotateY(0deg)";
+            flag = 0;
+            x = 0;
+        }
+     }, 100);
+}
+});
+dollarCoin.addEventListener("mouseleave",function(){
+     window.clearInterval(intervalID);
+    dollarCoin.style.WebkitTransform = "rotateY(0deg)";
+
 });
 
 
 
-// var mainDiv = document.getElementById("mainDiv");
-// var char = document.getElementById("char");
-
-// var hr = document.createElement("hr");
-// class character {
-// 	constructor (right, left, top, bottom) {
-//         this.right = right;
-//         this.left = left;
-//         this.top = top;
-//         this.bottom = bottom;
-//     }
-//     setRight(right) {
-//         this.right = right;
-//     } 
-//     setTop(top) {
-//         this.top = top;
-//     }
-//     getRight(){
-//     	return this.right;
-//     }
-//     getTop(){
-//     	return this.top;
-//     }
-
-// }
-
-// chObj = new character();
-
-// mainDiv.insertBefore(hr,char);
-// char.style.width="100px";
-// char.style.right="50%";
-// char.style.bottom="0px";
-// bottom=0;
-// var right = parseInt((Math.random()*100)+10);
-// var random = parseInt((Math.random()*10)+3);
-// if(random > 8 )random -=2;
+var mainDiv = document.getElementById("mainDiv");
+var char = document.getElementById("char");
 
 
-// while(random--){
-// 	iceImg = document.createElement("img");
-// 	iceImg.style ="position:absolute;width: 70px;";
-//  	iceImg.src = "imgs/ice.png"; 
-//  	iceImg.style.right=right+"%";
-// 	iceImg.style.bottom="30%";
-//  	mainDiv.appendChild(iceImg);
-//  	right-=5;
-//  }
-//  document.addEventListener('keydown',function(e){
-//  	if(e.keyCode == 32){
-//  		var x = 0;
-// 		var intervalID = setInterval(function () {
+char.style.width="100px";
+char.style.right="50%";
+char.style.bottom="0px";
+bottom=0;
 
-// 		   // Your logic here
-//  		char.style.bottom = (bottom+=5)+"%"
+	iceImg = document.getElementById("coin");
+	iceImg.style ="position:absolute;width: 70px;";
+ 	iceImg.style.right="50%";
+	iceImg.style.bottom="30%";
+ 	mainDiv.appendChild(iceImg);
 
-// 		   if (++x === 8) {
-// 		       window.clearInterval(intervalID);
-// 		   }
-// 		}, 100);
-//  	}else if(e.keyCode == 37){
+ 
+ document.addEventListener('keydown',function(e){
+ 	if(e.keyCode == 32){
+ 		var x = 0;
+		var intervalID = setInterval(function () {
+
+		   // Your logic here
+ 		char.style.bottom = (bottom+=5)+"%";
+        coin.style.WebkitTransform = "rotate("+rotate+"deg)"; 
+        rotate+=20;
+
+
+		   if (++x === 8) {
+		       window.clearInterval(intervalID);
+               iceImg.style.display = "none";
+		   }
+		}, 100);
+ 	}else if(e.keyCode == 37){
  		
-//  	}
+ 	}
 
-//  });
+ });
 
 
 
